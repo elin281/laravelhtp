ad5a5b44d84c9fb113538c46f631943b966433cb
arip86 <akhmadarip86@gmail.com>
2023-05-30 11:42:37 +0700

    penambahan chart pada dashboard

--- public/admin/assets/demo/chart-bar-demo.js
+++ public/admin/assets/demo/chart-bar-demo.js
@@ -3,16 +3,17 @@ Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSyste
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
 // Bar Chart Example
+
 var ctx = document.getElementById("myBarChart");
 var myLineChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Januari", "February", "March", "April", "May", "June"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [6000, 5312, 6251, 7841, 9821, 14984],
     }],
   },
   options: {
